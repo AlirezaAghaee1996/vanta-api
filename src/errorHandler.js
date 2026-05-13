07a59c557d9a08a79c8a27d7abba0dedc12667ab
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2026-05-13 14:25:57 +0330

    fix bug

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,8 +1,6 @@
 const catchError=(err,req,res,next)=>{
     err.statusCode=err.statusCode || 500
-    err.status=err.status||'error'  
     res.status(err.statusCode).json({
-        status:err.status,
         success:false,
         message:err.message
     })
